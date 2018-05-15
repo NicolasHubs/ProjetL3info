e46226800bc860eacbccf17855228f3451761671
ArthurBoury <arthur.boury@laposte.net>
2018-05-15 15:15:46 +0200

    coucou

---
+++ Assets/tmp/Caracteristic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caracteristic : MonoBehaviour {
+
+    bool showPopup = true;
+    public string textAffich;
+
+
+    void OnMouseEnter()
+    {
+        textAffich = "bonjour caca";
+    }
+
+}

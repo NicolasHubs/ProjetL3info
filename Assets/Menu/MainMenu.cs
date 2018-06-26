b611fa0f64141e7ec442fe269b9a87998211791c
oZeelite <alex35foot@gmail.com>
2018-06-26 14:07:51 +0200

    Menu principale avec animation etoile

---
+++ Assets/Menu/MainMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI; 
+
+public class MainMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown; 
+
+    Resolution[] resolutions; 
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        
+        List<string> options = new List<string>() ;
+
+        for (int i = 0; i < resolutions.Length; i++){
+           string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option); 
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("MasterVolume", volume); 
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex); 
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen ; 
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen); 
+    }
+}
+
+
---
+++ Assets/Menu/animation/etoileRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class etoileRotation : MonoBehaviour {
+
+
+    public float rotateX = 1.0f;
+    public float rotateY = 0f;
+    public float rotateZ = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotateX > 0)
+        {
+            transform.Rotate(rotateX * Time.deltaTime , 0f, 0f); 
+        }
+        if (rotateY > 0)
+        {
+            transform.Rotate(0f, rotateY * Time.deltaTime, 0f);
+        }
+        if (rotateZ > 0)
+        {
+            transform.Rotate( 0f, 0f, rotateZ * Time.deltaTime );
+        }
+    }
+}
